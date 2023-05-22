0b1ebc44abf47b017d26af8875ad7ad2a4ea7b8f
ObaidKazi <obaidkazi03@gmail.com>
2023-05-22 16:12:29 +0530

    added second way

--- object_detection.py
+++ object_detection.py
@@ -1,36 +1,41 @@
 import cv2
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
-
-#image=cv2.imread('image/computer.jpg')
+from ultralytics import YOLO
+image=cv2.imread('image/computer.jpg')
 #image=cv2.imread('image/bike.jpeg')
 #image=cv2.imread('image/car.jpg')
 #image=cv2.imread('image/cup.jpg')
 #image=cv2.imread('image/bottle.jpg')
 #image=cv2.imread('image/cat.jpg')
 #image=cv2.imread('image/fruit.png')
-image=cv2.imread('image/dog-and-cat-cover.jpg')
+#image=cv2.imread('image/dog-and-cat-cover.jpg')
     
-bbox, label, conf = cv.detect_common_objects(image)
+#bbox, label, conf = cv.detect_common_objects(image,confidence=0.7)
+# output_image = draw_bbox(image, bbox, label, conf)
 
-output_image = draw_bbox(image, bbox, label, conf)
+# cv2.namedWindow('Detection', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('Detection', 800, 800)
 
-cv2.namedWindow('Detection', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Detection', 800, 800)
+# cv2.imshow('Detection', output_image)
 
-cv2.imshow('Detection', output_image)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
-# from imageai.Detection import ObjectDetection
-# detector = ObjectDetection()
-# detector.setModelTypeAsRetinaNet()
-# detector.setModelPath("resnet50_imagenet_tf.2.0.h5")
+#other way to detect object 
 
-# detector.loadModel()
+model = YOLO("yolov8x.pt")
 
-# detections = detector.detectObjects
-# FromImage(input_image="image/dog-and-cat-cover.jpg", output_image_path="bike.jpg", minimum_percentage_probability=30)
-# print(detections)
+results=model.predict(source=image,show=True) 
+names = model.names
+
+for r in results:
+    for c in r.boxes.cls:
+        print(names[int(c)])
+
+while True:
+    k = cv2.waitKey(1) & 0xFF
+    # press 'q' to exit
+    if k == ord('q'):
+        break
